c9a245003771d181122e41f3fe20f80fcb30053f
arthas168 <pavlovskipetko@abv.bg>
2019-12-29 08:58:07 +0200

    added welcome text and anim to it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "../styles/main.scss";
 import Header from "./Header";
+import underline from "../images/home/underline.png";
 
 export default function Home() {
   return (
     <section id="home">
       <div className="veil"></div>
       <Header />
+      <div className="welcome flicker-in-1">
+        <span>Uniquely Dazzling</span>
+        <img src={underline} alt="white-underline"></img>
+      </div>
     </section>
   );
 }
