cf594e5ebebff6dd618a8461238ca8336805b099
arthas168 <pavlovskipetko@abv.bg>
2019-12-29 21:58:38 +0200

    added arrows to carousel

--- src/helpers/sliderParams.js
+++ src/helpers/sliderParams.js
@@ -1,10 +1,42 @@
-export default  {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    pauseOnHover: true,
+import React from "react";
+import nextArr from "../images/gallery/next-arr.png";
+import prevArr from "../images/gallery/prev-arr.png";
+
+function PrevArr(props) {
+  const { className, onClick } = props;
+  return (
+    <img
+      src={prevArr}
+      className={className}
+      onClick={onClick}
+      draggable="false"
+      alt="prev-arrow"
+    />
+  );
+}
+
+function NextArr(props) {
+  const { className, onClick } = props;
+  return (
+    <img
+      src={nextArr}
+      className={className}
+      onClick={onClick}
+      draggable="false"
+      alt="next-arrow"
+    />
+  );
+}
+
+export default {
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: true,
+  prevArrow: <PrevArr />,
+  nextArrow: <NextArr />
 };
