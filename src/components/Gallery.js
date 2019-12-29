a3edcb5d525798c8c99d6fd8cb587b6da64583cc
arthas168 <pavlovskipetko@abv.bg>
2019-12-29 08:03:40 +0200

    added basic placeholder sl slider

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,23 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import sliderParams from "../helpers/sliderParams";
+import ls from "../images/gallery/ls.jpg";
+import ls1 from "../images/gallery/ls1.jpg";
+import ls2 from "../images/gallery/ls2.jpg";
+import ls3 from "../images/gallery/ls3.jpg";
+
 
 export default function Gallery() {
-  return <section id="gallery"></section>;
+  return (
+    <section id="gallery">
+      <Slider {...sliderParams} className="slider">
+        <img src={ls} alt="placeholder"></img>
+        <img src={ls1} alt="placeholder"></img>
+        <img src={ls2} alt="placeholder"></img>
+        <img src={ls3} alt="placeholder"></img>
+      </Slider>
+    </section>
+  );
 }
---
+++ src/helpers/sliderParams.js
@@ -0,0 +1,10 @@
+export default  {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnHover: true,
+};
